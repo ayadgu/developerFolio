cd7641e7790f8d98a809b834f9002cdd4328c790
Guillaume Ayad <ayadguillaume@gmail.com>
2025-04-14 20:33:54 +0200

    readme fix

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,15 +10,11 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
+          {/* {emoji("Made with ❤️ by DeveloperFolio Team")} */}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
-          <a
-            href="https://github.com/saadpasta/developerFolio"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://github.com/ayadgu/" target="_blank" rel="noreferrer">
             developerFolio
           </a>
         </p>
